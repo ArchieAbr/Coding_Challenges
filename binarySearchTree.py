ac16fb1b8c9cac428af693550b0654094a62497a
Archie Abraham <archiepabraham@gmail.com>
2024-05-24 02:13:02 +0100

    Added a few intial TODOs and some basic code

---
+++ binarySearchTree.py
@@ -0,0 +1,11 @@
+# Challenge One: Create a Binary Search Tree
+
+# TODO: Create an array of integers to be searched
+
+array = [1, 2, 3, 4, 5, 6, 7, 68, 92, 1034, 256]
+
+# TODO: Let the user select a number
+userNumber = input("Please enter a the number you want to find in the array:/n")
+
+
+# TODO: Find the midpoint of the array and compare, if larger go right if smaller go left
