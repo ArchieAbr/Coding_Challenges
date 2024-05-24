3a4b557331038aff7bf1725c5a7f2f18a0e88edd
Archie Abraham <archiepabraham@gmail.com>
2024-05-24 02:17:06 +0100

    decided to practice using functions and added a debug print statement to test my code

--- binarySearchTree.py
+++ binarySearchTree.py
@@ -2,10 +2,15 @@
 
 # TODO: Create an array of integers to be searched
 
-array = [1, 2, 3, 4, 5, 6, 7, 68, 92, 1034, 256]
+array = [1, 2, 3, 4, 5, 6, 7, 68, 92, 1034, 256, 344]
 
 # TODO: Let the user select a number
-userNumber = input("Please enter a the number you want to find in the array:/n")
-
+def selection:
+    user_number = input("Please enter a the number you want to find in the array:/n")
+    return user_number
 
 # TODO: Find the midpoint of the array and compare, if larger go right if smaller go left
+def binary_search(user_number):
+    mid_point = len(array) / 2
+    print(mid_point)
+    return 0
