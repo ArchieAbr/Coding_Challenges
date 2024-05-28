d545744a3aa98f540980e0df1c894bc412c0d5e6
Archie Abraham <archiepabraham@gmail.com>
2024-05-28 12:47:05 +0100

    Added a function to convert the results into megabytes

--- Python/speedTest.py
+++ Python/speedTest.py
@@ -2,17 +2,24 @@ import speedtest
 
 s = speedtest.Speedtest()
 
+
+# Function to turn the bytes result into megabytes
+def bytes_to_mb(bytes):
+    kb = 1024
+    mb = kb * 1024
+    return int(bytes / mb)
+
+
 # Ping
 s.get_servers([])
 ping = s.results.ping
 
 # Download speed test
-download_speed = s.download()
+download_speed = bytes_to_mb(s.download())
 
 # Upload speed test
-upload_speed = s.upload()
+upload_speed = bytes_to_mb(s.upload())
 
-print("Your download speed is:", download_speed)
-print("Your upload speed is:", upload_speed)
+print("Your download speed is:", download_speed, "MB/s\n")
+print("Your upload speed is:", upload_speed, "MB/s\n")
 print("Your current ping is:", ping)
-
