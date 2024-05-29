9b31a22854e52aea83a7403efb0c446cdd7e9afc
Archie Abraham <archiepabraham@gmail.com>
2024-05-29 22:38:39 +0100

    Added round and formatted output

--- Python/findTheDiscount.py
+++ Python/findTheDiscount.py
@@ -6,15 +6,16 @@ def find_discount():
     price = float(input("Please entre the original price of the item:\n"))
     discount = float(input("Please enter the discount percentage:\n"))
     # Calculate discount
-    div_val = discount / 100
-    discounted_price = price - (price * div_val)
+    div_val = float(discount / 100)
+    discounted_price = float(price - (price * div_val))
     return discounted_price
 
 
 # Main
 def main():
-    discount = find_discount()
-    print("The discount is:", discount, "\n")
+    discount = float(find_discount())
+    discount = round(discount, 2)
+    print(f"The discounted price is: £{discount}\n")
     return 0
 
 
