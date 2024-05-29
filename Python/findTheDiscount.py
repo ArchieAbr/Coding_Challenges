cf088fa2778665f1541a78f3eaf6c76cfe4ab7a9
Archie Abraham <archiepabraham@gmail.com>
2024-05-29 22:59:26 +0100

    Added loop so that the user can keep using the program

--- Python/findTheDiscount.py
+++ Python/findTheDiscount.py
@@ -1,3 +1,6 @@
+from sys import exit
+
+
 def find_discount():
     price = float(input("Please entre the original price of the item:\n"))
     discount = float(input("Please enter the discount percentage:\n"))
@@ -9,9 +12,21 @@ def find_discount():
 
 # Main
 def main():
-    discount = float(find_discount())
-    discount = round(discount, 2)
-    print(f"The discounted price is: £{discount}\n")
+    main_loop = True
+    while main_loop:
+        discount = float(find_discount())
+        discount = round(discount, 2)
+        print(f"The discounted price is: £{discount}\n")
+        # Loop so that the user can reuse the program
+        while True:
+            choice = str(input("Do you want to calculate another discount?(y/n)\n"))
+            if choice == "y":
+                break
+            elif choice == "n":
+                exit("Program will now exit")
+            else:
+                print("Invalid input, please make a selection from the options provided(y/n)\n")
+
     return 0
 
 
