1c103db17973294a928d86ad6db88bdec03e6062
Archie Abraham <archiepabraham@gmail.com>
2024-06-21 19:27:17 +0100

    Fixed SSL issue

--- Python/youtubeToMP4.py
+++ Python/youtubeToMP4.py
@@ -1,6 +1,8 @@
+import ssl
 from pytube import YouTube
 
 # Set up
+ssl._create_default_https_context = ssl._create_unverified_context
 SAVE_PATH = "/Users/archie/Desktop/Youtube_Videos"
 link = input("Please copy the Youtube Video URL into the terminal\n")
 
@@ -10,6 +12,7 @@ def download(link):
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
+        print("Downloading video...\n")
         youtubeObject.download(output_path=SAVE_PATH)
     except:
         print("An error has occurred while trying to download your video, please check your network connection\n")
