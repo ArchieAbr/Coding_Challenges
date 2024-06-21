ffafe3d83842a5f8cde2d2bcb274816ebd4ff045
Archie Abraham <archiepabraham@gmail.com>
2024-06-21 17:04:26 +0100

    There is an issue with SSL CA verification preventing video downloads

--- Python/youtubeToMP4.py
+++ Python/youtubeToMP4.py
@@ -2,4 +2,18 @@ from pytube import YouTube
 
 # Set up
 SAVE_PATH = "/Users/archie/Desktop/Youtube_Videos"
-link = input("Please copy the Youtube Video URL into the terminal/n")
+link = input("Please copy the Youtube Video URL into the terminal\n")
+
+
+# Function to handle downloading
+def download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download(output_path=SAVE_PATH)
+    except:
+        print("An error has occurred while trying to download your video, please check your network connection\n")
+    print("Download completed successfully to:", SAVE_PATH, "\n")
+
+
+download(link)
