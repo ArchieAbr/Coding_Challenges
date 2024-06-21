29dedbcefb382263e59dd883dd0bdde8783eafa0
Archie Abraham <archiepabraham@gmail.com>
2024-06-21 19:56:39 +0100

    added a few more features inluding a structured main function and some other bits that I found in the pytube library to jazz this up a little

--- Python/youtubeToMP4.py
+++ Python/youtubeToMP4.py
@@ -1,22 +1,48 @@
 import ssl
+import sys
+import socket
 from pytube import YouTube
 
 # Set up
 ssl._create_default_https_context = ssl._create_unverified_context
 SAVE_PATH = "/Users/archie/Desktop/Youtube_Videos"
-link = input("Please copy the Youtube Video URL into the terminal\n")
+
+
+def test_connection():
+    try:
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        return False
 
 
 # Function to handle downloading
 def download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    youtube_object = YouTube(link)
+    youtube_object = youtube_object.streams.get_highest_resolution()
     try:
-        print("Downloading video...\n")
-        youtubeObject.download(output_path=SAVE_PATH)
+        title = youtube_object.title
+        file_size = youtube_object.filesize_mb
+        print("Found video:", title)
+        print("Approx. file size:", file_size, "MB")
+        print("Downloading Video...\n")
+        youtube_object.download(output_path=SAVE_PATH)
+        print("Download completed successfully, video is can be found here:", SAVE_PATH, "\n")
     except:
         print("An error has occurred while trying to download your video, please check your network connection\n")
-    print("Download completed successfully to:", SAVE_PATH, "\n")
+        sys.exit(1)
 
 
-download(link)
+
+def main():
+    if not test_connection():
+        print("No internet connection.")
+        return
+    print("Checking connection...")
+    print("Connection established.\n")
+    link = input("Please copy the Youtube Video URL into the terminal\n")
+    download(link)
+
+
+if __name__ == "__main__":
+    main()
